8561ac550d4d2012c5a606b68610d22852bc5485
mtsaryk <tsarykmarat@gmail.com>
2017-04-21 16:55:27 +0300

    third fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,287 @@
-$(document).ready(function(){
-	$(".mainNav_menuBtn").on("click",function(e){
-        e.preventDefault();
+$(document).ready(function () {
+    $(".mainNav_menuBtn").on("click", function (e) {
+        if (!e) {
+            e = window.event;
+        }
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+        toggleMenu();
+    });
+    $(".mainNav_drop_link").on("click", function (e) {
+        if (!e) {
+            e = window.event;
+        }
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+        toggleMenu();
+        slide(this);
+    });
+    $(".linkHome").on("click",function(e){
+        if (!e) {
+            e = window.event;
+        }
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+        slide(this);
+    })
+    function toggleMenu() {
         $(".mainNav_drop").toggleClass("active");
         $("body").toggleClass("active");
         $(".mainNav_menuBtn_line").toggleClass("active");
-    });
+    }
+
+    function slide(elem) {
+        let id = $(elem).attr("href"),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1000);
+    }
 })
+google.maps.event.addDomListener(window, 'load', init);
+var map, markersArray = [];
+
+function bindInfoWindow(marker, map, location) {
+    google.maps.event.addListener(marker, 'click', function () {
+        function close(location) {
+            location.ib.close();
+            location.infoWindowVisible = false;
+            location.ib = null;
+        }
+
+        if (location.infoWindowVisible === true) {
+            close(location);
+        } else {
+            markersArray.forEach(function (loc, index) {
+                if (loc.ib && loc.ib !== null) {
+                    close(loc);
+                }
+            });
+
+            var boxText = document.createElement('div');
+            boxText.style.cssText = 'background: #fff;';
+            boxText.classList.add('md-whiteframe-2dp');
+
+            function buildPieces(location, el, part, icon) {
+                if (location[part] === '') {
+                    return '';
+                } else if (location.iw[part]) {
+                    switch (el) {
+                        case 'photo':
+                            if (location.photo) {
+                                return '<div class="iw-photo" style="background-image: url(' + location.photo + ');"></div>';
+                            } else {
+                                return '';
+                            }
+                            break;
+                        case 'iw-toolbar':
+                            return '<div class="iw-toolbar"><h3 class="md-subhead">' + location.title + '</h3></div>';
+                            break;
+                        case 'div':
+                            switch (part) {
+                                case 'email':
+                                    return '<div class="iw-details"><i class="material-icons" style="color:#4285f4;"><img src="//cdn.mapkit.io/v1/icons/' + icon + '.svg"/></i><span><a href="mailto:' + location.email + '" target="_blank">' + location.email + '</a></span></div>';
+                                    break;
+                                case 'web':
+                                    return '<div class="iw-details"><i class="material-icons" style="color:#4285f4;"><img src="//cdn.mapkit.io/v1/icons/' + icon + '.svg"/></i><span><a href="' + location.web + '" target="_blank">' + location.web_formatted + '</a></span></div>';
+                                    break;
+                                case 'desc':
+                                    return '<label class="iw-desc" for="cb_details"><input type="checkbox" id="cb_details"/><h3 class="iw-x-details">Details</h3><i class="material-icons toggle-open-details"><img src="//cdn.mapkit.io/v1/icons/' + icon + '.svg"/></i><p class="iw-x-details">' + location.desc + '</p></label>';
+                                    break;
+                                default:
+                                    return '<div class="iw-details"><i class="material-icons"><img src="//cdn.mapkit.io/v1/icons/' + icon + '.svg"/></i><span>' + location[part] + '</span></div>';
+                                    break;
+                            }
+                            break;
+                        case 'open_hours':
+                            var items = '';
+                            if (location.open_hours.length > 0) {
+                                for (var i = 0; i < location.open_hours.length; ++i) {
+                                    if (i !== 0) {
+                                        items += '<li><strong>' + location.open_hours[i].day + '</strong><strong>' + location.open_hours[i].hours + '</strong></li>';
+                                    }
+                                    var first = '<li><label for="cb_hours"><input type="checkbox" id="cb_hours"/><strong>' + location.open_hours[0].day + '</strong><strong>' + location.open_hours[0].hours + '</strong><i class="material-icons toggle-open-hours"><img src="//cdn.mapkit.io/v1/icons/keyboard_arrow_down.svg"/></i><ul>' + items + '</ul></label></li>';
+                                }
+                                return '<div class="iw-list"><i class="material-icons first-material-icons" style="color:#4285f4;"><img src="//cdn.mapkit.io/v1/icons/' + icon + '.svg"/></i><ul>' + first + '</ul></div>';
+                            } else {
+                                return '';
+                            }
+                            break;
+                    }
+                } else {
+                    return '';
+                }
+            }
+
+            boxText.innerHTML =
+                buildPieces(location, 'photo', 'photo', '') +
+                buildPieces(location, 'iw-toolbar', 'title', '') +
+                buildPieces(location, 'div', 'address', 'location_on') +
+                buildPieces(location, 'div', 'web', 'public') +
+                buildPieces(location, 'div', 'email', 'email') +
+                buildPieces(location, 'div', 'tel', 'phone') +
+                buildPieces(location, 'div', 'int_tel', 'phone') +
+                buildPieces(location, 'open_hours', 'open_hours', 'access_time') +
+                buildPieces(location, 'div', 'desc', 'keyboard_arrow_down');
+
+            var myOptions = {
+                alignBottom: true,
+                content: boxText,
+                disableAutoPan: true,
+                maxWidth: 0,
+                pixelOffset: new google.maps.Size(-140, -40),
+                zIndex: null,
+                boxStyle: {
+                    opacity: 1,
+                    width: '280px'
+                },
+                closeBoxMargin: '0px 0px 0px 0px',
+                infoBoxClearance: new google.maps.Size(1, 1),
+                isHidden: false,
+                pane: 'floatPane',
+                enableEventPropagation: false
+            };
+
+            location.ib = new InfoBox(myOptions);
+            location.ib.open(map, marker);
+            location.infoWindowVisible = true;
+        }
+    });
+}
+
+function init() {
+    var mapOptions = {
+        center: new google.maps.LatLng(49.791623290852804, 24.055936053967333),
+        zoom: 16,
+        gestureHandling: 'auto',
+        fullscreenControl: false,
+        zoomControl: true,
+        disableDoubleClickZoom: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        scrollwheel: false,
+        streetViewControl: false,
+        draggable: true,
+        clickableIcons: false,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP,
+            backgroundColor: "red"
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [{
+            "featureType": "water",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "color": "#b5cbe4"
+            }]
+        }, {
+            "featureType": "landscape",
+            "stylers": [{
+                "color": "#efefef"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#83a5b0"
+            }]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#bdcdd3"
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#ffffff"
+            }]
+        }, {
+            "featureType": "poi.park",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#e3eed3"
+            }]
+        }, {
+            "featureType": "administrative",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "lightness": 33
+            }]
+        }, {
+            "featureType": "road"
+        }, {
+            "featureType": "poi.park",
+            "elementType": "labels",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "lightness": 20
+            }]
+        }, {}, {
+            "featureType": "road",
+            "stylers": [{
+                "lightness": 20
+            }]
+        }]
+    }
+    var mapElement = document.getElementById('mapkit-2194');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var locations = [
+        {
+            "title": "Elegant",
+            "address": "проспект Червоної Калини, 62-А, Львів, Львівська область, Украина, 79000",
+            "desc": "Your WEB Studio!",
+            "tel": "",
+            "int_tel": "",
+            "email": "",
+            "web": "",
+            "web_formatted": "",
+            "open": "",
+            "time": "",
+            "lat": 49.7919696,
+            "lng": 24.05606480000006,
+            "open_hours": "",
+            "marker": "http://s016.radikal.ru/i337/1704/6c/71d36e392d9f.png",
+            "iw": {
+                "address": true,
+                "desc": true,
+                "email": false,
+                "enable": true,
+                "int_tel": false,
+                "open": true,
+                "open_hours": true,
+                "photo": true,
+                "tel": false,
+                "title": true,
+                "web": false
+            }
+        }
+        ];
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            icon: locations[i].marker,
+            position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
+            map: map,
+            title: locations[i].title,
+            address: locations[i].address,
+            desc: locations[i].desc,
+            tel: locations[i].tel,
+            int_tel: locations[i].int_tel,
+            vicinity: locations[i].vicinity,
+            open: locations[i].open,
+            open_hours: locations[i].open_hours,
+            photo: locations[i].photo,
+            time: locations[i].time,
+            email: locations[i].email,
+            web: locations[i].web,
+            iw: locations[i].iw
+        });
+        markersArray.push(marker);
+
+        if (locations[i].iw.enable === true) {
+            bindInfoWindow(marker, map, locations[i]);
+        }
+    }
+    $(".map_footer").text(locations[0].address);
+}
